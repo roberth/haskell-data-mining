31382b26030a6254bf4c6c26286ba5b61fb91c65
Robert Hensing <robert@roberthensing.nl>
2011-11-30 16:06:38 +0100

    DecisionTree.hs: fix GenSep for pairs, add example db

--- src/Data/Mining/Classification/DecisionTree.hs
+++ src/Data/Mining/Classification/DecisionTree.hs
@@ -45,9 +45,9 @@ instance GenSep Char (SepSet Char) where gensep = gensepEq
 instance GenSep [Char] (SepSet [Char]) where gensep = gensepEq
 
 instance (GenSep a xa, GenSep b xb) =>
-          GenSep (a,b)   (Either a b)
-  where gensep (Left s) = gensep s . fst
-        gensep (Right t) = gensep t . snd
+          GenSep (a,b) (Either xa xb)
+  where gensep v = (map Left . gensep . map fst) v ++
+                  (map Right . gensep . map snd) v
 
 uniqSort :: (Ord a) => [a] -> [a]
 uniqSort = map head . group . sort
@@ -91,3 +91,35 @@ gini = nrange $ \a -> (a * (1.0 - a))
 
 db1 :: [(Int, Char)]
 db1 = [(12345,'c'),(1,'a'),(2345,'b'),(13,'a'),(451,'a'),(235,'b'),(46,'a'),(4,'a'),(235,'b'),(425,'b'),(436,'b'),(324,'b')]
+
+data MyClass = A | B | C
+                       deriving (Eq, Ord, Show)
+
+db2 :: [((Int, Int), MyClass)]
+db2 = [ ((4, 30), A)
+      , ((5,  9), A)
+      , ((1, 54), A)
+      , ((4,  6), A)
+      , ((0,  6), A)
+      , ((5, 10), A)
+      , ((4, 26), A)
+      , ((3, 51), A)
+        
+      , ((0, 77), B)
+      , ((3, 80), B)
+      , ((7, 51), B)
+      , ((2, 30), B)
+      , ((9, 77), B)
+      , ((5, 33), B)
+      , ((7, 48), B)
+      , ((3, 76), B)
+        
+      , (( 5, 66), C)
+      , ((11, 48), C)
+      , ((13, 68), C)
+      , ((11, 39), C)
+      , ((14, 47), C)
+      , (( 9, 50), C)
+      , (( 9, 39), C)
+      , (( 9, 61), C)
+      ]
